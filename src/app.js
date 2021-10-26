1ab1c92758b05c9809f541b58fb1c9a26df2c08e
phucLan183 <tpllazero@gmail.com>
2021-10-27 03:30:50 +0700

    Start project

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const config = require('./config');
-require(dotenv).config();
+require('dotenv').config();
 
 // Connect Database
 const connectDB = require('./common')
