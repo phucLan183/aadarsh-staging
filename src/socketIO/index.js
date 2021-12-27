6c6bfb3c010235e6b068a99fb309069b679cd502
daonghia2404 <daonghia2404@gmail.com>
2021-12-27 23:56:37 +0700

    Cors Socket

--- src/socketIO/index.js
+++ src/socketIO/index.js
@@ -7,6 +7,7 @@ const MemberModel = require('../models/Members');
 module.exports = (server) => {
   const io = socketIO(server, {
     cors: {
+      origin: "http://localhost:3000",
       methods: ["GET", "POST"],
       credentials: true
     }
