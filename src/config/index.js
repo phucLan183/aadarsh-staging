f0cdd0940df8d12e9643f8a95bbe25798ad690f2
phucLan183 <tpllazero@gmail.com>
2022-01-22 18:06:27 +0700

    Update: Ignore check env

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
 
 const envFound = dotenv.config()
-if (envFound.error) {
-  throw new Error(" Couldn't find .env file ")
-}
+// if (envFound.error) {
+//   throw new Error(" Couldn't find .env file ")
+// }
 
 module.exports = {
   port: parseInt(process.env.PORT, 10),
